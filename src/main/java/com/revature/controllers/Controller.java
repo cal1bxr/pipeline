9252f6a3a2b12c03840ee3ceae6f82704a880632
Matthew Jordan <91514803+cal1bxr@users.noreply.github.com>
2021-10-22 14:06:06 -0400

    Delete src directory

--- src/main/java/com/revature/controllers/Controller.java
+++
@@ -1,7 +0,0 @@
-package com.revature.controllers;
-
-import io.javalin.Javalin;
-
-public interface Controller {
-	public void addRoutes(Javalin app);
-}
--- src/main/java/com/revature/utils/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.revature.utils;
-
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException(String errorMessage, Throwable error){
-        super(errorMessage);
-    }
-}
